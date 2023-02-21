0e4f63a60c6c1d78894e1c468e89add74f5777c0
nastasia5624 <nastya.5624@mail.ru>
2023-02-21 21:13:47 +0400

    добавляю новую форму авторизации

---
+++ data.js
@@ -0,0 +1,12 @@
+<script language="javascript" type="text/javascript"><!--
+var d = new Date();
+
+var day=new Array("Воскресенье","Понедельник","Вторник",
+"Среда","Четверг","Пятница","Суббота");
+
+var month=new Array("января","февраля","марта","апреля","мая","июня",
+"июля","августа","сентября","октября","ноября","декабря");
+
+(day[d.getDay()]+" " +d.getDate()+ " " + month[d.getMonth()]
++ " " + d.getFullYear() + " г.");
+//--></script>
